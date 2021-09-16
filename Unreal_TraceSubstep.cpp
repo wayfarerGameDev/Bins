1dbf28cdb2034142dca625e5968d3cf7200a13c3
wayfarerGameDev <84999694+wayfarerGameDev@users.noreply.github.com>
2021-09-15 23:37:38 -0500

    Update Unreal_TraceSubstep.cpp

--- Unreal_TraceSubstep.cpp
+++ Unreal_TraceSubstep.cpp
@@ -1,4 +1,38 @@
-void UFluidCharacterHitSubsystem::Tick(float DeltaTime)
+//////////////////
+///.h
+
+USTRUCT(BlueprintType)
+struct FHitTask
+{
+    GENERATED_BODY()
+
+public:
+
+    UPROPERTY(BlueprintReadWrite)
+        USceneComponent* SceneComponentTransformToTrack;
+    UPROPERTY(BlueprintReadWrite)
+        EFluidCharacterHitTaskShape Shape;
+    UPROPERTY(BlueprintReadWrite)
+        FVector Extent;
+    UPROPERTY(BlueprintReadWrite)
+        int SubSteps;
+    FVector LocationAtLastFrame;
+    FRotator RotationAtLastFrame;
+};
+
+//////////////////
+///.cpp
+
+static TAutoConsoleVariable<int32> CVarHitTraceDubug(
+    TEXT("Hit.Trace.Debug"),
+    0,
+    TEXT("Turns hit trace debugging.\n")
+    TEXT("<=0: off \n")
+    TEXT("  1: on\n"),
+    ECVF_SetByConsole);
+
+
+void UHitSubsystem::Tick(float DeltaTime)
 {
     
     //Trace Variables
@@ -8,10 +42,11 @@ void UFluidCharacterHitSubsystem::Tick(float DeltaTime)
     
     //Frame Variables
     FVector FrameLocation = FVector(0,0,0);
-    FRotator FrameOrientation;
+    FRotator FrameRotation;
     
     //Substep Variables
     FVector SubStepLocation = FVector(0,0,0);
+    FRotator SubStepRotation;
     float SubStepAlpha = 0;
     
     //Do Hit Tasks
@@ -19,21 +54,22 @@ void UFluidCharacterHitSubsystem::Tick(float DeltaTime)
     {
         //Set StartEnd
         FrameLocation = HitTasks[i].SceneComponentTransformToTrack->GetComponentLocation();
-        FrameOrientation = HitTasks[i].SceneComponentTransformToTrack->GetComponentRotation();
+        FrameRotation = HitTasks[i].SceneComponentTransformToTrack->GetComponentRotation();
         
         // Do Substeps
-        for (int j = 0; j < HitTasks[i].SubSteps; j++)
+        for (float j = 0; j < HitTasks[i].SubSteps && j < 12; j++)
         {
             //Substep
-            SubStepAlpha = DeltaTime / HitTasks[i].SubSteps * j;
+            SubStepAlpha = j / HitTasks[i].SubSteps;
             SubStepLocation = FMath::LerpStable(HitTasks[i].LocationAtLastFrame, FrameLocation, SubStepAlpha);
-
+            SubStepRotation = FMath::LerpStable(HitTasks[i].RotationAtLastFrame, FrameRotation, SubStepAlpha);
+            
             //Box Trace
-            if (HitTasks[i].Shape == EFluidCharacterHitTaskShape::VE_Box)
-                bDidHit = UKismetSystemLibrary::BoxTraceMulti(GetWorld(),SubStepLocation, SubStepLocation, HitTasks[i].Extent,FrameOrientation, UEngineTypes::ConvertToTraceType(ECC_Pawn), false, ActorsToIgnoire,       CVarFluidCharacterHitTraceDubug.GetValueOnAnyThread() > 0 ? EDrawDebugTrace::ForDuration : EDrawDebugTrace::None, HitArray, false, FLinearColor::Red, FLinearColor::Red, 1);
+            bDidHit = UKismetSystemLibrary::BoxTraceMulti(GetWorld(),SubStepLocation, SubStepLocation, HitTasks[i].Extent,FrameRotation, UEngineTypes::ConvertToTraceType(ECC_Pawn), false, ActorsToIgnoire,CVarHitTraceDubug.GetValueOnAnyThread() > 0 ? EDrawDebugTrace::ForDuration : EDrawDebugTrace::None, HitArray, false, FLinearColor::Red, FLinearColor::Red, 1);
         }
         
         //Set
-        HitTasks[i].LocationAtLastFrame = FrameLocation ;
+        HitTasks[i].LocationAtLastFrame = FrameLocation;
+        HitTasks[i].RotationAtLastFrame = FrameRotation;
     }
 }
